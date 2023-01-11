79fbaa18652a13038f3dd923402603fdcf40322c
curso js <urtzisanchez.a@gmail.com>
2023-01-11 17:26:33 +0100

    Ejer2.js

---
+++ Ejercicios/Ejer2.js
@@ -0,0 +1,16 @@
+let name = ("Urtzi")
+let age = (35)
+let developerQuestion = true
+let birthday = (16, 4, 1987)
+let favoriteBook = {
+    titulo: "maneras de amar",
+    autor: "amir levine",
+    fecha: 2023,
+    url: "https://www.casadellibro.com/libro-maneras-de-amar/9788415870869/2800181?campaignid=6513745351&adgroupid=77964323933&feeditemid&targetid=dsa-454892120139&matchtype&network=g&device=c&devicemodel&ifmobile%3A%5Bmobile%5D&ifnotmobile%3A%5Bnotmobile%5D=%5Bnotmobile%5D&ifsearch%3A%5Bsearch%5D=%5Bsearch%5D&ifcontent%3A%5Bdisplay%5D&creative=638732303448&keyword&placement&target&adposition&gclid=CjwKCAiA2fmdBhBpEiwA4CcHzXgU64aLY4ZLbEPe4OZBMPpNO8bb1hJejihqt9b9Nmwu5xI_eqcvjhoC0pMQAvD_BwE"
+}
+
+console.log(name);
+console.log(age);
+console.log(developerQuestion);
+console.log(birthday);
+console.log(favoriteBook.autor);
